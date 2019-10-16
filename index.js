174bbe4b042bca9d404f568707c85f8c9b9ca43e
Ilkka Kivistö <ilkka.kivisto@gmail.com>
2019-10-16 20:10:35 +0300

    Exercise 3.11 puhelinluettelon full stack, frontend deployment

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ morgan.token('post-data', (req, res) => JSON.stringify(req.body))
 app.use(cors())
 app.use(bodyParser.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post-data'))
+app.use(express.static('build'))
 
 let persons = [
     {
