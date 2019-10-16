7ad289766f89f07e300865c79abf7f2a7869ce4a
Ilkka Kivistö <ilkka.kivisto@gmail.com>
2019-10-16 20:22:51 +0300

    uibuild

---
+++ build/precache-manifest.c6ea770f5fe93137a5f415c4e9c430b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f234a5f3ff49be3295ccf146daae58f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3564678c81ecfd00eec8",
+    "url": "/static/css/main.b0a7673b.chunk.css"
+  },
+  {
+    "revision": "362982fa92d07fdb2b75",
+    "url": "/static/js/2.2755dcdf.chunk.js"
+  },
+  {
+    "revision": "3564678c81ecfd00eec8",
+    "url": "/static/js/main.f7546845.chunk.js"
+  },
+  {
+    "revision": "bd15de2c81a3438b90f1",
+    "url": "/static/js/runtime~main.11bda596.js"
+  }
+]);
