237d5c5a464c094dcc7e112958090e5fbdd998db
Ilkka Kivistö <ilkka.kivisto@gmail.com>
2019-10-16 19:11:33 +0300

    Exercise 3.9 Backend to Heroku

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
+const cors = require('cors')
 
 morgan.token('post-data', (req, res) => JSON.stringify(req.body))
 
+app.use(cors())
 app.use(bodyParser.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post-data'))
 
@@ -89,7 +91,7 @@ app.post('/api/persons', (req, res) => {
     res.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
