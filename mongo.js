6ce82dfcf875f9529c540646ab04c20d083dc98c
Ilkka Kivistö <ilkka.kivisto@gmail.com>
2019-10-21 17:00:09 +0300

    Exercise 3.12 tietokanta komentoriviltä

---
+++ mongo.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose')
+
+if ( process.argv.length<3 ) {
+    console.log('give password as argument')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+    `mongodb+srv://fullstack:${password}@cluster0-txopd.mongodb.net/puhelinluettelo-app?retryWrites=true&w=majority`
+
+mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true })
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length === 5) {
+    const newName = process.argv[3]
+    const newNumber = process.argv[4]
+    const person = new Person({
+        name: newName,
+        number: newNumber,
+    })
+
+    person.save().then(response => {
+        console.log(`added ${newName} number ${newNumber} to phonebook`);
+        mongoose.connection.close();
+    })
+} else if (process.argv.length === 3) {
+    console.log('phonebook:')
+    Person
+        .find({})
+        .then(persons => {
+            persons.forEach(person => {
+                console.log(person.name, person.number)
+            })
+            mongoose.connection.close()
+        })
+} else {
+    console.log('unknown arguments')
+    mongoose.connection.close()
+}
